6ef5e683dfc1812bf2db42aa19a7f14fa6ea04bc
Rensuhendra <Rendysuhendra09@gmail.com>
2022-03-28 18:06:12 +0700

    menambahkan .gitignore,application/,composer.json,contributing.md,index.php,license.txt,readme.rst,system/,user_guide/

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+ {
+    echo "<h1> Perkenalkan </h1>";
+    echo"Nama saya Imam Nawawi saya tinggal di ciputat olahraga yang saya sukai adalah sepak bola";
+ }
+}
