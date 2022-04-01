f8d8108147b1c76e1852344044707916a52dabb2
Rensuhendra <102407576+Rensuhendra@users.noreply.github.com>
2022-04-01 09:59:46 +0700

    Add files via upload

---
+++ view_latihan.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web Programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
+    ini asil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . 
+        $nilai2 . " = " . $hasil; ?>
+        </body>
+        </html>
